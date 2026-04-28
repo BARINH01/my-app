7b4a15a41fdf1ced1e6ff8c130cb451a57305f70
BARINH01 <ibaric00@fesb.hr>
2026-04-28 08:14:24 +0000

    Add Calculator app with unit test

--- my-java-app/src/main/java/com/myapp/App.java
+++ my-java-app/src/main/java/com/myapp/App.java
@@ -1,13 +1,13 @@
 package com.myapp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        
+        System.out.println("=== Kalkulator ===");
+        System.out.println("5 + 3 = " + calc.add(5, 3));
+        System.out.println("10 - 4 = " + calc.subtract(10, 4));
+        System.out.println("6 * 7 = " + calc.multiply(6, 7));
+        System.out.println("15 / 3 = " + calc.divide(15, 3));
     }
 }
